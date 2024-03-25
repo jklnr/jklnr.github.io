d53147e2b8503e862d1cfb1a7c77888f9ff4b4f4
Jay Kelner <jkelner@payitgov.com>
2024-03-24 21:31:30 -0500

    add something to show login worked (or not)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
 import "./App.css";
 import LogoutButton from "./LogoutButton";
 import LoginButton from "./LoginButton";
+import React, { useEffect, useState } from "react";
+
 import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
-  const { user, isAuthenticated, isLoading } = useAuth0();
+  const { user, isLoading, getAccessTokenSilently } = useAuth0();
+
+  // jklnr-api-73ea6b412c87.herokuapp
+  const [helloMessageResponse, setHelloMessageResponse] = useState(null);
+
+  useEffect(() => {
+    const getUserMetadata = async () => {
+      const domain = "jklnr-api-73ea6b412c87.herokuapp.com";
+
+      try {
+        const accessToken = await getAccessTokenSilently({
+          authorizationParams: {
+            audience: `https://${domain}/`,
+            scope: "read:current_user",
+          },
+        });
+
+        const helloMessageUrl = `https://${domain}/api/v1/hello`;
+
+        const helloMessageResponse = await fetch(helloMessageUrl, {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+          },
+        });
+
+        const { helloMessageResponse_ } = await helloMessageResponse.json();
+
+        setHelloMessageResponse(helloMessageResponse_);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+
+    getUserMetadata();
+  }, [getAccessTokenSilently, user?.sub]);
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -12,11 +48,9 @@ function App() {
 
   return (
     <div className="App">
-      {isAuthenticated ? (
+      {helloMessageResponse ? (
         <>
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
+          {helloMessageResponse}
           <LogoutButton></LogoutButton>
         </>
       ) : (
